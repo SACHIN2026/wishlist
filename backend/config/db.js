bbade7887dfe9eb5f58236139e1cb5f8f3573446
Sachin <sachsaini2004@gmail.com>
2025-05-03 21:55:48 +0530

    Refactor formatting in database connection function

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,13 @@
-import e from "express";
 import mongoose from "mongoose";
 
-const connectDB = async () =>{
+const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI)
-        console.log(`MongoDB Connected: ${conn.connection.host}`);   
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
 
     } catch (error) {
         console.log("Error connecting to MongoDB:", error.message);
-        
+
     }
 }
 
